a872ecf9f17549bcb72be421315339450b667b96
Gabriel Madrid <gabrielmadrid73@gmail.com>
2025-02-15 19:24:43 -0300

    feature/iac - Removed iac from repo and added k8s manifest

--- infra/rds/prod/aws_db_subnet_group.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_db_subnet_group" "main" {
-  name       = var.project
-  subnet_ids = concat(local.aws_public_subnets, local.aws_private_subnets)
-}
--- infra/rds/prod/output.tf
+++
@@ -1,3 +0,0 @@
-output "rds_public_sg_id" {
-  value = aws_security_group.rds_public_sg.id
-}
